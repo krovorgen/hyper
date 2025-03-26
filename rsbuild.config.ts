abaaebc70b3652b0f4a68e4653664fb23f6903c1
maksimberezka <krovorgen@gmail.com>
2025-03-26 11:24:13 +0300

    fix

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -7,5 +7,11 @@ export default defineConfig({
   plugins: [pluginReact(), pluginSass(), pluginSvgr()],
   html: {
     title: 'Hyper',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: 'Hyper Audience — запустите мобильную рекламу с любым таргетингом' },
+      { name: 'keywords', content: 'Hyper Audience, аналитика, аудитория, платформа, маркетинг' },
+    ],
   },
 });
